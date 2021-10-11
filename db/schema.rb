70cbf27c43ecb04740a99e2b120f19db3f703b1c
Tim Quimbo <tquimbo1@gmail.com>
2021-10-11 13:52:54 -0400

    commit tree 0525450307b237841d8562b23600b5ab8e50845a

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230232412) do
+ActiveRecord::Schema.define(version: 2015_12_30_232412) do
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
